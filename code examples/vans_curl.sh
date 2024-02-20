25405901face02cc6aecbb95a0f5c1e33e166f6c
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2024-02-20 14:58:35 +0200

    Adding code examples/vans_curl.sh

---
+++ code examples/vans_curl.sh
@@ -0,0 +1,4 @@
+curl --user user:pass \
+'https://realtime.oxylabs.io/v1/queries' \
+-H "Content-Type: application/json" \
+-d '{"source": "universal_ecommerce", "url": "https://www.vans.ca/en-ca/categories/sale-c5410?f=gender,women&gad_source=1&gclid=cjwkcaiaungubhakeiwagid4auzyymbv7pagm7evcvelpcwxj9iaurxiyfu5skvbpwyrcj2n2q08ahocyjqqavd_bwe"}'
